2c753c99005b0e55ca0a07af34216a265f2b8539
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-31 05:31:30 +0400

    Rollback

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,5 +10,6 @@ if app_views is not None:
     from api.v1.views.index import *
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
     from api.v1.views.states import *
     from api.v1.views.users import *
